5220307b1cce01257967d84b835b786faade4bac
Alexey Chuprin <maddogsstyle@gmail.com>
2022-07-04 23:02:29 +0300

    WIN

--- packer/scripts/install_mongodb.sh
+++ packer/scripts/install_mongodb.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 apt-get update
+sleep 50
 apt-get install -y mongodb
 systemctl start mongodb
 systemctl enable mongodb
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -12,9 +12,6 @@ variable public_key_path {
   # Описание переменной
   description = "Path to the public key used for ssh access"
 }
-variable image_id {
-  description = "Disk image"
-}
 variable subnet_id {
   description = "Subnet"
 }
@@ -27,4 +24,12 @@ variable private_key_path {
 variable copies {
   description = "number of instances"
   default     = "1"
+}
+variable app_disk_image {
+  description = "Disk image for reddit app"
+  default     = "reddit-app-base"
+}
+variable db_disk_image {
+  description = "Disk image for reddit db"
+  default     = "reddit-db-base"
 }
