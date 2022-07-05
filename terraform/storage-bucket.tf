f62ca2367a65098b2e5394685168bf1d14ffa6ab
Alexey Chuprin <maddogsstyle@gmail.com>
2022-07-06 01:16:56 +0300

    workflow pass?

--- terraform/storage-bucket.tf
+++ terraform/storage-bucket.tf
@@ -1,8 +1,8 @@
 provider "yandex" {
   service_account_key_file = var.service_account_key_file
-  cloud_id  = var.cloud_id
-  folder_id = var.folder_id
-  zone      = var.zone
+  cloud_id                 = var.cloud_id
+  folder_id                = var.folder_id
+  zone                     = var.zone
 }
 
 resource "yandex_storage_bucket" "state-storage" {
