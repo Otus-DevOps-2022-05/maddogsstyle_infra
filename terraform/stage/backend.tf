6c76003417ef5db6f48fa55c81cb0f5b01fa81db
Alexey Chuprin <maddogsstyle@gmail.com>
2022-07-05 23:55:40 +0300

    zadanie so * - win

---
+++ terraform/stage/backend.tf
@@ -0,0 +1,12 @@
+terraform {
+  backend "s3" {
+    endpoint = "storage.yandexcloud.net"
+    bucket   = "maddogsstyle"
+    region   = "ru-central1"
+    key      = "states/stage.tfstate"
+    access_key                  = "YCAJEdF2kE2H7joxAcIuSDJ-g"
+    secret_key                  = "YCP0vPA7DB0IPLedFO3HAKGqkGfNKwkx_BWo2QEg"
+    skip_region_validation      = true
+    skip_credentials_validation = true
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,22 @@
+variable cloud_id {
+  description = "Cloud"
+}
+variable folder_id {
+  description = "Folder"
+}
+variable zone {
+  description = "Zone"
+  default     = "ru-central1-a"
+}
+variable access {
+  description = "access"
+}
+variable security {
+  description = "security"
+}
+variable name {
+  description = "name"
+}
+variable service_account_key_file {
+  description = "key .json"
+}
